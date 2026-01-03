fd8fa56fa85655607649d82343dd6e18c7d6b6be
zia <asdlalsrud@naver.com>
2026-01-03 14:12:26 +0900

    Time: 4 ms (42.88%), Space: 43.1 MB (72.6%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+
+            if(c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            }
+            else {
+                if(stack.isEmpty()) return false;
+
+                char p = stack.pop();
+                if(c == ')' && p != '(' || c == '}' && p != '{' || c == ']' && p != '[') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
