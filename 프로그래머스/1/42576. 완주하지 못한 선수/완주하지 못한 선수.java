219c835566780f6a184a701f2820411f3a74186b
zia <asdlalsrud@naver.com>
2026-01-04 14:14:33 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 94.30 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,16 +4,16 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> hashtable = new HashMap<>();
         
-        for(String p : participant){
-            hashtable.put(p, hashtable.getOrDefault(p,0) + 1);
+        for(String p : participant) {
+            hashtable.put(p, hashtable.getOrDefault(p,0)+1);
         }
         
-        for(String c : completion){
+        for(String c : completion) {
             hashtable.put(c, hashtable.get(c) - 1);
         }
         
         for(String key : hashtable.keySet()) {
-            if(hashtable.get(key) == 1) return key;
+            if(hashtable.get(key) != 0) return key;
         }
         
         return null;
