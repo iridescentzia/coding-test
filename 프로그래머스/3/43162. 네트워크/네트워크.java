5ae4a206407d4257469fb93dabf82736d3f09335
zia <asdlalsrud@naver.com>
2026-01-05 15:03:07 +0900

    [level 3] Title: 네트워크, Time: 0.45 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -5,25 +5,25 @@ class Solution {
         boolean[] visited = new boolean[n];
         int cnt = 0;
         
-        for(int i = 0; i < n; i++){
-            if(!visited[i]){
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]) {
                 BFS(i, computers, visited);
                 cnt++;
             }
         }
+        
         return cnt;
     }
-    
-    private void BFS(int start, int[][] computers, boolean[] visited){
+    private void BFS(int start, int[][] computers, boolean[] visited) {
         Queue<Integer> queue = new ArrayDeque<>();
         queue.offer(start);
         visited[start] = true;
         
-        while(!queue.isEmpty()){
+        while(!queue.isEmpty()) {
             int current = queue.poll();
             
-            for(int next = 0; next < computers.length; next++){
-                if(computers[current][next] == 1 && !visited[next]){
+            for(int next = 0; next < computers.length; next++) {
+                if(computers[current][next] == 1 && !visited[next]) {
                     visited[next] = true;
                     queue.offer(next);
                 }
